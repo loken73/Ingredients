813a249a29447ed0f93defca7513cbf215dcb878
Frank <lokenator73@gmail.com>
2018-03-03 12:55:47 -0500

    Add project files.

---
+++ RecipeFinder/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeFinder.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ RecipeFinder/Models/Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeFinder.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+        public string Instructions { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+}
